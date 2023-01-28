04b685cd999d5f94e70efe5a1889f435cd6aedbb
mylinx <380440561@qq.com>
2023-01-29 00:07:48 +0800

    增加接口！

---
+++ GD.CMS.Service/ServiceAdmin/Logs/ILogService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using FreeSql;
+using GD.CMS.Common;
+using GD.CMS.Entity;
+
+namespace GD.CMS.Service
+{
+    public interface ILogService : IBaseService<LogsEntity> 
+    {
+
+        /// <summary>
+        /// 获取列表
+        /// </summary>
+        /// <returns></returns>
+        Task<LayResult> GetLayAsync(LogListCrud logListCrud);
+
+
+        Task<AjaxResult> Del(int id);
+    }
+}
